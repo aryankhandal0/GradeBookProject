35b2a6631116e3480db9ca58c69ca8d74aeed2dd
Aryan Khandal <aryankhandal0@gmail.com>
2020-05-08 17:23:11 +0530

    "In Chapter 8"

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -7,12 +7,32 @@ namespace GradeBook
     {
         public Book(string name)
         {
+            // category = "";
             grades = new List<double>();
             Name = name;
         }
         public void AddGrade(double grade)
         {
-            grades.Add(grade);
+            if(grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid {nameof(grade)}");
+            }
+        }
+        public void AddGrade(char letter)
+        {
+            switch(letter)
+            {
+                case 'A': AddGrade(90);break;
+                case 'B': AddGrade(80);break;
+                case 'C': AddGrade(70);break;
+                case 'D': AddGrade(60);break;
+                case 'E': AddGrade(50);break;
+                default: AddGrade(0);break;
+            }
         }
         public Statistics GetStatistics()
         {
@@ -20,17 +40,103 @@ namespace GradeBook
             result.Average = 0.0;
             result.High = double.MinValue;
             result.Low = double.MaxValue;
-            foreach(var grade in grades){
+            /* For Each Loop */
+            foreach(var grade in grades)
+            {
+                /* Break Statement */
+                // if(grade == 42.1)
+                // {
+                //     break;
+                // }
+
+                /* Continue Statement */
+                // if(grade == 42.1)
+                // {
+                //     continue;
+                // }
+
                 result.High = Math.Max(grade, result.High);
                 result.Low = Math.Min(grade, result.Low);
                 result.Average += grade;
             }
+            /* Do While Loop */
+            // var index = 0;
+            // do(){
+            //     result.High = Math.Max(grades[index], result.High);
+            //     result.Low = Math.Min(grades[index], result.Low);
+            //     result.Average += grades[index];
+            //     index += 1;
+            // }while(index < grades.Count);
+
+            /*  While Loop */
+            // while(index < grades.Count)
+            // {
+            //     result.High = Math.Max(grades[index], result.High);
+            //     result.Low = Math.Min(grades[index], result.Low);
+            //     result.Average += grades[index];
+            //     index += 1;
+            // }
+
+            /* For Loop */
+            // for(var index = 0;index < grades.Count; index++)
+            // {
+            //     result.High = Math.Max(grades[index], result.High);
+            //     result.Low = Math.Min(grades[index], result.Low);
+            //     result.Average += grades[index];
+            // }
             result.Average /= grades.Count;
+            switch(result.Average)
+            {
+                case var d when d>=90.0:
+                    result.Letter = 'A';
+                    break;
+                case var d when d>=80.0:
+                    result.Letter = 'B';
+                    break;
+                case var d when d>=70.0:
+                    result.Letter = 'C';
+                    break;
+                case var d when d>=60.0:
+                    result.Letter = 'D';
+                    break;
+                case var d when d>=50.0:
+                    result.Letter = 'E';
+                    break;
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
             return result;     
         }
         private List<double> grades;
-        public string Name;
-
         
+        /* Property Write Getter-Setter Complex Code*/
+        /*
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if(string.IsNullOrEmpty(value))
+                {
+                    name = value;
+                }
+            }
+        }
+        private string name;
+        */
+        /* Property Write Getter-Setter Easy Code*/
+        public string Name
+        {
+            get;
+            set;
+            // Blocking Writes
+            // private set;
+        }
+        // readonly string category = "Science";
+        // public const string CATEGORY = "Science";
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,13 +9,45 @@ namespace GradeBook
         {
             
             var book = new Book("Aryan Grade Book");
-            book.AddGrade(89.1);
+            /* book.AddGrade(89.1);
             book.AddGrade(90.1);
-            book.AddGrade(77.5);
+            book.AddGrade(77.5); */
+            while(true)
+            {
+                System.Console.WriteLine("Enter a Grade or press 'q' to quit");
+                var input = Console.ReadLine();
+                if(input == "q")
+                {
+                    break;
+                }
+                try
+                {
+                var grade = double.Parse(input);
+                book.AddGrade(grade);
+                }
+                catch(ArgumentException ex)
+                {
+                    System.Console.WriteLine(ex.Message);
+                }
+                catch(FormatException ex)
+                {
+                    System.Console.WriteLine(ex.Message);
+                }
+                // finally
+                // {
+                //     System.Console.WriteLine("**");   
+                // }
+            }
+            
             var stats = book.GetStatistics();
+            book.Name = "";
+            System.Console.WriteLine($"For the book {book.Name}");
             System.Console.WriteLine($"The average grade is {stats.Average:N2}!");
             System.Console.WriteLine($"The highest grade is {stats.High:N2}!");
             System.Console.WriteLine($"The lowest grade is {stats.Low:N2}!");
+            System.Console.WriteLine($"The letter grade is {stats.Letter}!");
+
+
         }
     }
 }
